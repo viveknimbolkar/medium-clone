f52601b8ca74de3dda21846316bb5b7ea53f01cd
viveknimbolkar <viveknimbolkar50@gmail.com>
2023-08-02 23:41:29 +0530

    profile models

--- components/settings/Account.js
+++ components/settings/Account.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from 'react'
 import Modal from '../modal';
 import Image from 'next/image';
 import UserImage from '../../public/user.png'
+import Link from 'next/link';
 
 
 function EmailModalBody({ onClose }) {
@@ -89,11 +90,30 @@ function MutedWriterModalBody() {
 function BlockedUserModalBody() {
   return <></>
 }
-function DeactivateAccountModalBody() {
-  return <></>
+function DeactivateAccountModalBody({ onClose }) {
+  return <>
+    <p className='my-4'>
+      Deactivating your account will remove it from Medium within a few minutes. Deactivation will also immediately cancel any subscription for Medium Membership, and no money will be reimbursed. You can sign back in anytime to reactivate your account and restore its content.
+    </p>
+    <div className='mt-5 text-end'>
+      <button onClick={onClose} className='border-2 px-3 py-2 w-24 rounded-full mr-4'>Cancel</button>
+      <button className='border-2 px-3 py-2 w-24 rounded-full text-white bg-green-400 hover:bg-green-500 duration-100 border-green-400 hover:border-green-500'>Save</button>
+    </div>
+  </>
 }
-function DeleteAccountModalBody() {
-  return <></>
+function DeleteAccountModalBody({ onClose }) {
+  return <>
+    <p className='my-4'>
+      We’re sorry to see you go. Once your account is deleted, all of your content will be permanently gone, including your profile, stories, publications, notes, and responses. Deleting your Medium account will not delete any Stripe account you have connected to your Medium account. If you’re not sure about that, we suggest you deactivate or submit a request at <Link href={"/"} className='underline'> help.medium.com </Link> instead.
+      If you created a Medium Membership through the Apple App store, you must also cancel your subscription via iTunes.
+
+      To confirm deletion, type “delete” below:
+    </p>
+    <div className='mt-5 text-end'>
+      <button onClick={onClose} className='border-2 px-3 py-2 w-24 rounded-full mr-4'>Cancel</button>
+      <button className='border-2 px-3 py-2 w-24 rounded-full text-white bg-green-400 hover:bg-green-500 duration-100 border-green-400 hover:border-green-500'>Save</button>
+    </div>
+  </>
 }
 
 function Account() {
@@ -104,15 +124,19 @@ function Account() {
     <div className='text-sm'>
       <div onClick={(e) => { setActiveModal('email'); setIsModalOpen(!isModalOpen); }} className='py-3 cursor-pointer flex justify-between'>
         <h5 className=''>Email address</h5>
-        <span className='cursor-pointer'>sssssssssssssssssssssssss</span>
+        <span className='cursor-pointer'>viveknimbolkar@gmail.com</span>
       </div>
       <div onClick={(e) => { setActiveModal('username'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
         <h5>Username & Subdomain</h5>
-        <span>sssssssssssssssssssssssss</span>
+        <span>@vivek</span>
       </div>
       <div onClick={(e) => { setActiveModal('profile-info'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
-        <h5>Profile Information</h5>
-        <span>sssssssssssssssssssssssss</span>
+        <div>
+
+          <h5>Profile Information</h5>
+          <div className='text-xs text-gray-600'>Edit your photo, name, bio, etc.</div>
+        </div>
+        <span className='flex items-center gap-3 text-gray-600'><h4>Vivek</h4> <Image src={UserImage} width={30} height={300} /></span>
       </div>
       <div onClick={(e) => { setActiveModal('profile-design'); setIsModalOpen(!isModalOpen);; }} className='cursor-pointer py-3 flex justify-between'>
         <h5>Profile Design</h5>
@@ -120,25 +144,25 @@ function Account() {
       </div>
       <div onClick={(e) => { setActiveModal('muted-writer'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
         <h5>Muted Writers & Publicators</h5>
-        <span>sssssssssssssssssssssssss</span>
+
       </div>
       <div onClick={(e) => { setActiveModal('blocked-user'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
         <h5>Blocked User</h5>
-        <span>sssssssssssssssssssssssss</span>
+
       </div>
       <div onClick={(e) => { setActiveModal('deactivate-account'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
         <div>
           <h5 className='text-red-500'>Deactivate Account</h5>
           <span>Deactivating will suspend your account until you sign back in.</span>
         </div>
-        <span>sssssssssssssssssssssssss</span>
+
       </div>
       <div onClick={(e) => { setActiveModal('delete-account'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
         <div>
           <h5 className='text-red-500'>Delete Account</h5>
           <span>Permanently delete your account and all of your content.</span>
         </div>
-        <span>sssssssssssssssssssssssss</span>
+
       </div>
 
       <Modal isOpened={activeModal === 'email'} onClose={() => setActiveModal(!activeModal)} header={'Email address'} modalBody={<EmailModalBody onClose={() => setActiveModal(!activeModal)} />}>
@@ -150,16 +174,16 @@ function Account() {
       <Modal isOpened={activeModal === 'profile-info'} onClose={() => setActiveModal(!activeModal)} header={'Profile Information'} modalBody={<ProfileInfoModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
       </Modal>
 
-      <Modal isOpened={activeModal === 'delete-account'} onClose={() => setActiveModal(!activeModal)} header={'Email address'} modalBody={<EmailModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
+      <Modal isOpened={activeModal === 'delete-account'} onClose={() => setActiveModal(!activeModal)} header={'Delete Account'} modalBody={<DeleteAccountModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
       </Modal>
 
       <Modal isOpened={activeModal === 'muted-writer'} onClose={() => setActiveModal(!activeModal)} header={'Email address'} modalBody={<EmailModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
       </Modal>
 
-      <Modal isOpened={activeModal === 'deactivate-account'} onClose={() => setActiveModal(!activeModal)} header={'Email address'} modalBody={<EmailModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
+      <Modal isOpened={activeModal === 'deactivate-account'} onClose={() => setActiveModal(!activeModal)} header={'Deactivate account'} modalBody={<DeactivateAccountModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
       </Modal>
 
-      <Modal isOpened={activeModal === 'blocked-user'} onClose={() => setActiveModal(!activeModal)} header={'Email address'} modalBody={<EmailModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
+      <Modal isOpened={activeModal === 'blocked-user'} onClose={() => setActiveModal(!activeModal)} header={'Block Account'} modalBody={<BlockedUserModalBody onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
       </Modal>
 
 
--- components/settings/Publishing.js
+++ components/settings/Publishing.js
@@ -1,8 +1,129 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import Modal from '../modal';
+import Image from 'next/image';
+import UserImage from '../../public/user.png'
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+
+function ReplyEmail({ onClose }) {
+
+  const [emailUpdate, setEmailUpdate] = useState('');
+
+  return <>
+    <input value={emailUpdate} onChange={e => setEmailUpdate(e.target.value)} type='email' className='mt-5 focus:outline-none border-b border-black w-full' />
+    <p className='text-xs mt-1 text-gray-500'>You can sign into Medium with this email address.</p>
+    <div className='mt-5 text-end'>
+      <button onClick={onClose} className='border-2 px-3 py-2 w-24 rounded-full mr-4'>Cancel</button>
+      <button className='border-2 px-3 py-2 w-24 rounded-full text-white bg-green-400 hover:bg-green-500 duration-100 border-green-400 hover:border-green-500'>Save</button>
+    </div></>
+}
+
+function ManagePublication({ onClose }) {
+  return <>
+    <Link href={"/"} className='text-green-600 flex gap-2 items-center text-center justify-center my-5'>
+      <FontAwesomeIcon icon={faPlus} /> <h5>Create a publication</h5>
+    </Link>
+    <p>You don't belong to any publications.</p>
+  </>
+}
+function ManageTripping({ onClose }) {
+  return <>
+    <p className='my-5 text-sm text-gray-500'>Allow readers to send a tip by adding a link to your preferred payment platform, which will appear at the bottom of your stories.</p>
+    <hr />
+    <div className='mt-5 text-end'>
+      <button onClick={onClose} className='border-2 px-3 py-2 w-24 rounded-full mr-4'>Cancel</button>
+      <button className='border-2 px-3 py-2 w-24 rounded-full text-white bg-green-400 hover:bg-green-500 duration-100 border-green-400 hover:border-green-500'>Save</button>
+    </div>
+  </>
+}
+function CustomizeSubscription({ onClose }) {
+
+  return <>
+    <p>Allow readers to send a tip by adding a link to your preferred payment platform, which will appear at the bottom of your stories.</p>
+    <hr />
+    <div className='mt-5 text-end'>
+      <button onClick={onClose} className='border-2 px-3 py-2 w-24 rounded-full mr-4'>Cancel</button>
+      <button className='border-2 px-3 py-2 w-24 rounded-full text-white bg-green-400 hover:bg-green-500 duration-100 border-green-400 hover:border-green-500'>Save</button>
+    </div>
+  </>
+}
+
 
 function Publishing() {
+  const [activeModal, setActiveModal] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [allowEmailReplies, setAllowEmailReplies] = useState(false);
+
   return (
-    <div>Publishing</div>
+    <div className='text-sm'>
+      <div onClick={(e) => { setActiveModal('manage-publication'); setIsModalOpen(!isModalOpen); }} className='py-3 cursor-pointer flex justify-between'>
+        <h5 className=''>Manage publications</h5>
+        <span className='cursor-pointer'>viveknimbolkar@gmail.com</span>
+      </div>
+
+      <div onClick={(e) => { setActiveModal('manage-tripping'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
+        <div>
+          <h5>Manage tipping on your stories</h5>
+          <div className='text-xs text-gray-600'>Readers can send you tips through the third-party platform of your choice.</div>
+        </div>
+        <input type='checkbox' className='w-5 h-5' />
+      </div>
+
+      <div onClick={(e) => { setAllowEmailReplies(!allowEmailReplies); setIsModalOpen(!isModalOpen); }} className={`${allowEmailReplies ? 'cursor-not-allowed text-gray-400' : ""} cursor-pointer py-3 flex justify-between`}>
+        <div>
+          <h5>Allow email replies
+          </h5>
+          <div className='text-xs text-gray-600'>
+            Let readers reply to your stories directly from their email.</div>
+        </div>
+        <input value={allowEmailReplies} onChange={e => { setAllowEmailReplies(e.target.checked) }} type='checkbox' className='w-5 h-5' />
+      </div>
+
+      <div onClick={(e) => { setActiveModal('reply-email'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
+        <div>
+          <h5>Reply's To Email
+          </h5>
+          <div className='text-xs text-gray-600'>
+            Let readers reply to your stories directly from their email.</div>
+        </div>
+      </div>
+
+      <div onClick={(e) => { setActiveModal('blocked-user'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
+        <h5>Blocked User</h5>
+
+      </div>
+
+      <div onClick={(e) => { setActiveModal('deactivate-account'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
+        <div>
+          <h5 className='text-red-500'>Deactivate Account</h5>
+          <span>Deactivating will suspend your account until you sign back in.</span>
+        </div>
+
+      </div>
+
+      <div onClick={(e) => { setActiveModal('delete-account'); setIsModalOpen(!isModalOpen); }} className='cursor-pointer py-3 flex justify-between'>
+        <div>
+          <h5 className='text-red-500'>Delete Account</h5>
+          <span>Permanently delete your account and all of your content.</span>
+        </div>
+
+      </div>
+
+      <Modal isOpened={activeModal === 'manage-publication'} onClose={() => setActiveModal(!activeModal)} header={'Manage Publications'} modalBody={<ManagePublication onClose={() => setActiveModal(!activeModal)} />}>
+      </Modal>
+
+      <Modal isOpened={activeModal === 'reply-email'} onClose={() => setActiveModal(!activeModal)} header={'‘Reply To’ email address'} modalBody={<ReplyEmail onClose={() => setActiveModal(!activeModal)} />}>
+      </Modal>
+
+      <Modal isOpened={activeModal === 'customize-subscription'} onClose={() => setActiveModal("")} header={'‘Reply To’ email address'} modalBody={<CustomizeSubscription onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
+      </Modal>
+
+      <Modal isOpened={activeModal === 'manage-tripping'} onClose={() => setActiveModal(!activeModal)} header={'Manage tipping on your stories'} modalBody={<ManageTripping onClose={() => { setIsModalOpen(!isModalOpen); setActiveModal("") }} />}>
+      </Modal>
+
+    </div>
   )
 }
 
